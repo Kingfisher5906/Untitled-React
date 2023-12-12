c7635308ed4b3a24b66b133da04087d4f4b0eb47
Kingfisher5906 <89612814+Kingfisher5906@users.noreply.github.com>
2023-12-12 11:34:28 -0500

    Commit from TeleportHQ

---
+++ src/views/desktop1.js
@@ -0,0 +1,303 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './desktop1.css'
+
+const Desktop1 = (props) => {
+  return (
+    <div className="desktop1-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="desktop1-desktop1">
+        <div className="desktop1-nav-bar">
+          <span className="desktop1-text">
+            <span>SchedGen Client</span>
+          </span>
+          <span className="desktop1-text02">
+            <span>Dashboard</span>
+          </span>
+          <span className="desktop1-text04">
+            <span>Courses</span>
+          </span>
+          <span className="desktop1-text06">
+            <span>
+              <span>Analysis</span>
+              <br></br>
+              <span></span>
+            </span>
+          </span>
+          <img alt="Ellipse11456" src className="desktop1-ellipse1" />
+        </div>
+        <div className="desktop1-organizer-frame">
+          <div className="desktop1-gpa-frame">
+            <span className="desktop1-text11">
+              <span>GPA</span>
+            </span>
+            <span className="desktop1-text13">
+              <span>4.79</span>
+            </span>
+          </div>
+          <div className="desktop1-frame2">
+            <div className="desktop1-frame3">
+              <span className="desktop1-text15">
+                <span>Course Grades Overview</span>
+              </span>
+            </div>
+          </div>
+          <div className="desktop1-extra-info-frame"></div>
+          <div className="desktop1-group1">
+            <div className="desktop1-class-box">
+              <span className="desktop1-text17">
+                <span>
+                  Computer Aided Design and 3D Printing H - 1 (C)
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+              <span className="desktop1-text19">
+                <span>Benjamin Weidenaar</span>
+              </span>
+              <img
+                alt="Line11464"
+                src="/external/line11464-f9m.svg"
+                className="desktop1-line1"
+              />
+              <span className="desktop1-text21">
+                <span>
+                  <span>99.99%</span>
+                  <br></br>
+                  <span></span>
+                </span>
+              </span>
+              <img
+                alt="Line21465"
+                src="/external/line21465-of9q.svg"
+                className="desktop1-line2"
+              />
+              <span className="desktop1-text26">
+                <span>A+</span>
+              </span>
+            </div>
+            <div className="desktop1-class-box1">
+              <span className="desktop1-text28">
+                <span>
+                  Computer Aided Design and 3D Printing H - 1 (C)
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+              <span className="desktop1-text30">
+                <span>Benjamin Weidenaar</span>
+              </span>
+              <img
+                alt="Line11627"
+                src="/external/line11627-zfj8.svg"
+                className="desktop1-line11"
+              />
+              <span className="desktop1-text32">
+                <span>
+                  <span>99.99%</span>
+                  <br></br>
+                  <span></span>
+                </span>
+              </span>
+              <img
+                alt="Line21629"
+                src="/external/line21629-xptn.svg"
+                className="desktop1-line21"
+              />
+              <span className="desktop1-text37">
+                <span>A+</span>
+              </span>
+            </div>
+            <div className="desktop1-class-box2">
+              <span className="desktop1-text39">
+                <span>
+                  Computer Aided Design and 3D Printing H - 1 (C)
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+              <span className="desktop1-text41">
+                <span>Benjamin Weidenaar</span>
+              </span>
+              <img
+                alt="Line11634"
+                src="/external/line11634-c7p.svg"
+                className="desktop1-line12"
+              />
+              <span className="desktop1-text43">
+                <span>
+                  <span>99.99%</span>
+                  <br></br>
+                  <span></span>
+                </span>
+              </span>
+              <img
+                alt="Line21636"
+                src="/external/line21636-nnhm.svg"
+                className="desktop1-line22"
+              />
+              <span className="desktop1-text48">
+                <span>A+</span>
+              </span>
+            </div>
+            <div className="desktop1-class-box3">
+              <span className="desktop1-text50">
+                <span>
+                  Computer Aided Design and 3D Printing H - 1 (C)
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+              <span className="desktop1-text52">
+                <span>Benjamin Weidenaar</span>
+              </span>
+              <img
+                alt="Line11641"
+                src="/external/line11641-zxpn.svg"
+                className="desktop1-line13"
+              />
+              <span className="desktop1-text54">
+                <span>
+                  <span>99.99%</span>
+                  <br></br>
+                  <span></span>
+                </span>
+              </span>
+              <img
+                alt="Line21643"
+                src="/external/line21643-d0k.svg"
+                className="desktop1-line23"
+              />
+              <span className="desktop1-text59">
+                <span>A+</span>
+              </span>
+            </div>
+            <div className="desktop1-class-box4">
+              <span className="desktop1-text61">
+                <span>
+                  Computer Aided Design and 3D Printing H - 1 (C)
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+              <span className="desktop1-text63">
+                <span>Benjamin Weidenaar</span>
+              </span>
+              <img
+                alt="Line11648"
+                src="/external/line11648-uf.svg"
+                className="desktop1-line14"
+              />
+              <span className="desktop1-text65">
+                <span>
+                  <span>99.99%</span>
+                  <br></br>
+                  <span></span>
+                </span>
+              </span>
+              <img
+                alt="Line21650"
+                src="/external/line21650-xhc.svg"
+                className="desktop1-line24"
+              />
+              <span className="desktop1-text70">
+                <span>A+</span>
+              </span>
+            </div>
+            <div className="desktop1-class-box5">
+              <span className="desktop1-text72">
+                <span>
+                  Computer Aided Design and 3D Printing H - 1 (C)
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+              <span className="desktop1-text74">
+                <span>Benjamin Weidenaar</span>
+              </span>
+              <img
+                alt="Line11655"
+                src="/external/line11655-jmfr.svg"
+                className="desktop1-line15"
+              />
+              <span className="desktop1-text76">
+                <span>
+                  <span>99.99%</span>
+                  <br></br>
+                  <span></span>
+                </span>
+              </span>
+              <img
+                alt="Line21657"
+                src="/external/line21657-oei4.svg"
+                className="desktop1-line25"
+              />
+              <span className="desktop1-text81">
+                <span>A+</span>
+              </span>
+            </div>
+            <div className="desktop1-class-box6">
+              <span className="desktop1-text83">
+                <span>
+                  Computer Aided Design and 3D Printing H - 1 (C)
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+              <span className="desktop1-text85">
+                <span>Benjamin Weidenaar</span>
+              </span>
+              <img
+                alt="Line11662"
+                src="/external/line11662-1x1c.svg"
+                className="desktop1-line16"
+              />
+              <span className="desktop1-text87">
+                <span>
+                  <span>99.99%</span>
+                  <br></br>
+                  <span></span>
+                </span>
+              </span>
+              <img
+                alt="Line21664"
+                src="/external/line21664-lhpb.svg"
+                className="desktop1-line26"
+              />
+              <span className="desktop1-text92">
+                <span>A+</span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Desktop1
